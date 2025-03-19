63f1f13c86e3fc6f4907fdf3f36d03f72cd2c866
figo123123333 <filip.lukas@infis.cz>
2025-03-19 13:06:51 +0100

    Update ukol 2.py

---
+++ ukol 2.py
@@ -0,0 +1,12 @@
+cislo1 = float(input("zadejte první desetiné číslo"))
+cislo2 = float(input("zadejte druhé desetiné čislo"))
+
+soucet = cislo1 + cislo2
+rozdil = cislo1 - cislo2
+soucin = cislo1 * cislo2
+podil = cislo1 / cislo2
+
+print("součet je"(soucet))
+print("rozdil je"(rozdil))
+print("součin je"(soucin))
+print("podíl je"(podil))
